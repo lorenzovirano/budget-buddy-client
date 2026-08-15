4afbdbd4d0767c1667c1aa877d2d338c493c4136
Lorenzo Virano <lorenzovirano1702@gmail.com>
2026-08-15 13:46:34 +0200

    fix: minor fix for local implementation

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -2,7 +2,7 @@ import axios from "axios"
 import { useAuthStore } from "@/stores/useAuthStore"
 
 export const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL, 
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:8080", 
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
